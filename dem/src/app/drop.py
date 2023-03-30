Fixed particle-particle interaction, need to fix computation of tangential displacement and compute tangential forces

dem/src/app/drop.py
@@ -16,7 +16,7 @@ class drop(base_app):
         super().__init__()
 
         self.name      = 'drop'
-        self.t_max     = 6.0
+        self.t_max     = 10.0
         self.dt        = 0.00002
         self.nt        = int(self.t_max/self.dt)+1
         self.plot_freq = 1000
@@ -28,19 +28,19 @@ class drop(base_app):
         young   = 210.0e9 # steel
         poisson = 0.25    # steel
 
-        self.p = particles(n           = 100,
+        self.n_row = 25 # nb of particles on a row at start
+        self.n_col = 35 # nb of particles on a col at start
+        self.radius = 0.025
+
+        self.p = particles(n           = self.n_row*self.n_col,
                            nt          = self.nt,
                            density     = density,
-                           radius      = 0.05,
-                           restitution = 1.0,
+                           radius      = self.radius,
+                           restitution = 0.9,
                            young       = young,
                            poisson     = poisson,
                            color       = "b",
                            store       = False)
-
-        # Restitution ratios
-        self.p.e[:] = 0.5
-        self.p.set_particles()
 
         # Colors
         colors = np.array(['r', 'g', 'b', 'c', 'm', 'y', 'k'])
@@ -64,11 +64,12 @@ class drop(base_app):
     def reset(self):
 
         self.t = 0.0
+        sep = 4.0*self.radius
 
-        for i in range(10):
-            for j in range(10):
-                self.p.x[10*i+j,0] = 0.1 + 0.2*i + 0.05*random.random()
-                self.p.x[10*i+j,1] = 2.0 + 0.2*j
+        for i in range(self.n_row):
+            for j in range(self.n_col):
+                self.p.x[self.n_col*i+j,0] = sep + sep*i + self.radius*random.random()
+                self.p.x[self.n_col*i+j,1] = 10*sep + sep*j
 
     ### ************************************************
     ### Compute forces

dem/src/core/collision.py
@@ -5,6 +5,9 @@ import numba as nb
 
 ### ************************************************
 ### Compute forces given input parameters of collision
+### Penetration dx is an absolute distance
+### Normals must be from 1 towards 2
+### Relative velocity is computed as v12 = v2-v1
 @nb.njit(cache=True)
 def hertz(dx, r1, r2, m1, m2, v1, v2, n, t,
           g1, g2, Eb1, Eb2, Gb1, Gb2):
@@ -14,6 +17,7 @@ def hertz(dx, r1, r2, m1, m2, v1, v2, n, t,
     m  = 1.0/(1.0/m1 + 1.0/m2)
     eb = 1.0/(Eb1 + Eb2)
     gb = 1.0/(Gb1 + Gb2)
+    v  = v2 - v1
 
     # normal stiffness
     k_n  = (4.0/3.0)*np.sqrt(r)*eb
@@ -28,8 +32,8 @@ def hertz(dx, r1, r2, m1, m2, v1, v2, n, t,
     nu_t = g1*np.sqrt(k_t*m)
 
     # normal and tangential velocities
-    vn   = v1[0]*n[0] + v1[1]*n[1]
-    vt   = v1[0]*t[0] + v1[1]*t[1]
+    vn   = v[0]*n[0] + v[1]*n[1]
+    vt   = v[0]*t[0] + v[1]*t[1]
 
     # forces
     fne = np.zeros((2))
@@ -52,7 +56,7 @@ def hertz(dx, r1, r2, m1, m2, v1, v2, n, t,
     #p.f[i,:] -= pow(dx,0.5)*k_t*vt*0.00001*t[:]
 
     # tangential damping force
-    ftd[0] =-dx025*nu_t*vt*t[0]
-    ftd[1] =-dx025*nu_t*vt*t[1]
+    #ftd[0] =-dx025*nu_t*vt*t[0]
+    #ftd[1] =-dx025*nu_t*vt*t[1]
 
     return fne, fnd, fte, ftd

dem/src/domain/rectangle.py
@@ -44,10 +44,14 @@ class rectangle(base_domain):
         self.c = np.array([-self.y_min,-self.x_max,
                            -self.y_max,-self.x_min])
         self.d = np.array([1.0, 1.0, 1.0, 1.0])
+
+        # Inward normals
         self.n = np.array([[ 0.0, 1.0],
                            [-1.0, 0.0],
                            [ 0.0,-1.0],
                            [ 1.0, 0.0]])
+
+        # Trigonometric direction tangents
         self.t = np.array([[ 1.0, 0.0],
                            [ 0.0, 1.0],
                            [-1.0, 0.0],
@@ -125,19 +129,19 @@ def rectangle_forces(f, p_r, p_m, p_v, p_g, p_Eb, p_Gb, dx,
                                    p_r[i],   d_r,     # radii
                                    p_m[i],   d_m,     # masses
                                    p_v[i,:], d_v,     # velocities
-                                   n[j,:],   t[j,:],  # normal and tangent
+                                  -n[j,:],   t[j,:],  # normal and tangent
                                    p_g[i],   d_g,     # restitution coeffs
                                    p_Eb[i],  d_Eb[j], # Eb coeffs
                                    p_Gb[i],  d_Gb[j]) # Gb coeffs
 
         # normal elastic force
-        f[i,:] += fne[:]
+        f[i,:] -= fne[:]
 
         # normal damping force
-        f[i,:] += fnd[:]
+        f[i,:] -= fnd[:]
 
         # tangential elastic force
-        f[i,:] += fte[:]
+        #f[i,:] += fte[:]
 
         # tangential damping force
-        f[i,:] += ftd[:]
+        #f[i,:] += ftd[:]